6ac2ff49efd5a1c9c2829f3d3cb5892122e81dfb
snekxl <zedandkiller@gmail.com>
2026-08-04 23:38:50 +0700

    Update ftp_parser.h

--- client/ftp_parser.h
+++ client/ftp_parser.h
@@ -1,6 +1,16 @@
-#pragma once
-#include <string>
-#include <winsock2.h> // Bổ sung thư viện này để nhận diện kiểu SOCKET
+#ifndef FTP_PARSER_H
+#define FTP_PARSER_H
 
-// Truyền SOCKET vào để bộ phân tích biết đường gửi lệnh
+#include <winsock2.h>
+#include <string>
+
+// Chạy vòng lặp nhập lệnh từ bàn phím
 void runCommandParser(SOCKET control_sock);
+
+// Hàm phụ trợ gửi lệnh (có tự động thêm \r\n theo chuẩn FTP)
+bool sendCommand(SOCKET sock, const std::string& cmd);
+
+// Hàm phụ trợ nhận phản hồi từ Server
+std::string receiveReply(SOCKET sock);
+
+#endif
