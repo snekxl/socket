abe35e7aa7178b3cdfec1adfe9e5d858f62d400c
snekxl <zedandkiller@gmail.com>
2026-08-17 01:29:12 +0700

    Update ftp_parser.h

--- client/ftp_parser.h
+++ client/ftp_parser.h
@@ -4,13 +4,8 @@
 #include <winsock2.h>
 #include <string>
 
-// Chạy vòng lặp nhập lệnh từ bàn phím, truyền thêm serverIP cho luồng UDP
 void runCommandParser(SOCKET control_sock, const std::string& serverIP);
-
-// Hàm phụ trợ gửi lệnh
 bool sendCommand(SOCKET sock, const std::string& cmd);
-
-// Hàm phụ trợ nhận phản hồi từ Server
 std::string receiveReply(SOCKET sock);
 
 #endif
