bbec545c10e2f84de670ae8932f003c4be902253
snekxl <zedandkiller@gmail.com>
2026-08-17 01:27:45 +0700

    Update network_tcp.h

--- client/network_tcp.h
+++ client/network_tcp.h
@@ -5,16 +5,9 @@
 #include <ws2tcpip.h>
 #include <string>
 
-// Khởi tạo thư viện Winsock (Bắt buộc trên Windows)
 bool initWinsock();
-
-// Tạo socket TCP cho kênh điều khiển (Control Channel)
 SOCKET createTCPSocket();
-
-// Kết nối đến Server thông qua IP và Port
 bool connectToServer(SOCKET sock, const std::string& serverIP, int port);
-
-// Đóng kết nối TCP và dọn dẹp bộ nhớ
 void closeTCPSocket(SOCKET sock);
 
 #endif
